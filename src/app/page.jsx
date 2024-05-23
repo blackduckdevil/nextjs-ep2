ce17b9cc944d78b63b83853d75d3ba6715afba7a
blackduckdevil <blackduckdevil@gmail.com>
2024-05-23 21:29:17 +0700

    PokeData commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Header from "./components/Header";
-import Pokedata from "./components/Pokedata";
+import Pokedata from "./components/PokeData";
 
 export default function Home() {
   return (
